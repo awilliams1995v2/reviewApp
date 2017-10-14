98966a4a1b1c6404969680c25ed3ceddfdec2248
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2017-10-14 00:46:01 -0400

    socket

--- server2.js
+++ server2.js
@@ -15,25 +15,6 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 
 var PORT = process.env.PORT || 8080;
 
-var cookieSession = require('cookie-session')
-var express = require('express')
-
-var app = express()
-
-app.set('trust proxy', 1) // trust first proxy
-
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2']
-}))
-
-app.get('/', function (req, res, next) {
-  // Update views
-  req.session.lastVisit = (req.session.views || 0) + 1
-
-  // Write response
-  res.end(req.session.views + ' views')
-})
 
 // Requiring our models for syncing
 // Sets up the Express app to handle data parsing
@@ -114,7 +95,7 @@ app.get('/auth/facebook/callback',
     console.log("in callback to authentication route below");
     console.log(req.user);
 /*    db.OnlineUser.findOne({where:{UserfbId : req.user.fbId}})
-*/    res.redirect("/channelRendering")
+*/    res.redirect("/"+req.session)
   });
 
 
